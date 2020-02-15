46abdadc57d1e038dcfd4370a42e882b4f0dfcaa
oliverhua <hua.oliver@gmail.com>
2020-02-15 15:24:51 +0800

    0215

--- project/main.py
+++ project/main.py
@@ -8,21 +8,22 @@ import time
 
 url = "https://data.nhi.gov.tw/Datasets/Download.ashx?rid=A21030000I-D50001-001&l=https://data.nhi.gov.tw/resource/mask/maskdata.csv"
 
-headers = {
-    "cookie": "CONSENT=YES+GB.zh-TW+20150803-20-0; SID=tgcUAXYOR6oFj5lAvcw3O-hj0kfvOte0c-qxfuqt_BcDpQlnKtZ1mEpSsa9bMGtsw8qf0w.; __Secure-3PSID=tgcUAXYOR6oFj5lAvcw3O-hj0kfvOte0c-qxfuqt_BcDpQln-TqmLIajBloKEfasYq63kA.; HSID=Ap8eYih2aKxp_-R08; SSID=AhrL54ug2Md28NAr-; APISID=RLZidnRS24PvVrte/AdJXbcKtT-AvzK7et; SAPISID=UW7sVtpr1fUqynEX/AuKDEqqnKN-KepntB; __Secure-HSID=Ap8eYih2aKxp_-R08; __Secure-SSID=AhrL54ug2Md28NAr-; __Secure-APISID=RLZidnRS24PvVrte/AdJXbcKtT-AvzK7et; __Secure-3PAPISID=UW7sVtpr1fUqynEX/AuKDEqqnKN-KepntB; NID=198=sx71LiI-6Zr-W2lsys3539dFDp_FVvtT_C32jjAsJk-ky3ptgCfQiHm2Lrr-eeRin7yj39UGmKL3TtgGQeKa2BfQYd1WpZwA0u0cv0vvVZkHvd8rylv6gzi5GSI42Ijh62WhauX5MO7BA79HYidr2sQroKlBwUHwr2O1y9wcg6IhvGHzJPR4f-dRC_g8ADsEfRmPCTQ8e_xdLk_0o_AN77KpRDv55xQmyYU0a_dHdhKA8cEreNaxnxA36ByY2iGugr4QamAW0GFYjOY2h29zGCkAtoxfPfZ3z_zSoU-u6NxQ2HQpInKKAcqqPo7kLcDYYD_AMFadlrJCRQFwSQj0Dl6qwZQr; GOOGLE_ABUSE_EXEMPTION=ID=7a810cf3c7f11dae:TM=1581661992:C=r:IP=2001:b400:e2db:95c5:547f:29bd:43be:b272-:S=APGng0sPZnkOrkFCS14O3Rwl9tD17ritqw; 1P_JAR=2020-2-14-6",
-    "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.100 Safari/537.36",
-    "x-client-data": "CKm1yQEIlbbJAQijtskBCMS2yQEIqZ3KAQicoMoBCMuuygEIvLDKAQiXtcoBCO21ygEIjrrKARirpMoBGIqyygE=",
-    "dnt" : "1",
-    "referer": "https://www.google.com.tw/"
-}
+cnt = 1
 error_cnt = 0
 dic = []
 demo_dic = []
 address_dic = {}
 demo_address_dic = {}
+tmp_dic = {}
 # print(rows[0])
 def convert_string(full):
     s = full
+    s = s.replace("Ｆ","F")
+    s = s.replace("Ｃ","C")
+    s = s.replace("－","-")
+    s = s.replace("（","(")
+    s = s.replace("）",")")
+    s = s.replace("：",":")
     s = s.replace("０","0")
     s = s.replace("１","1")
     s = s.replace("２","2")
@@ -36,7 +37,11 @@ def convert_string(full):
     return s
     
 def get_coor(addr):
-    browser = webdriver.Chrome(executable_path='chromedriver')
+    global cnt
+    cnt += 1
+    options = webdriver.ChromeOptions()
+    options.add_argument("headless")
+    browser = webdriver.Chrome(executable_path='chromedriver',options=options)
     browser.get("http://www.map.com.tw/")
     search = browser.find_element_by_id("searchWord")
     search.clear()
@@ -51,8 +56,15 @@ def get_coor(addr):
     cor = coor.text.strip().split(" ")
     lat = cor[-1].split("：")[-1]
     log = cor[0].split("：")[-1]
-    browser.close()
+    browser.quit()
     return (lat, log)
+
+threads = []
+# for i in range(5):
+#   threads.append(threading.Thread(target = job, args = (i,)))
+#   threads[i].start()
+
+# def job(url):
 
 def update_mask():
     global demo_address_dic
@@ -62,18 +74,20 @@ def update_mask():
     rows = list(csv_data)
     for row in rows:
         row[2] = convert_string(row[2])
-        if error_cnt >= 100:
-            return
         if not row[2] in address_dic:
-            address_dic[row[2]] = get_coor(row[2]) #return tuple
-            print("Find New", row[2], address_dic[row[2]])
-
+                address_dic[row[2]] = get_coor(row[2]) #return tuple
+                print("Find New", row[2], address_dic[row[2]])
+                if cnt % 5 == 0:
+                    print("[cnt]"+str(cnt))
+                    save_data()
         elif address_dic[row[2]][0] == 0 and address_dic[row[2]][1] == 0:
-            address_dic[row[2]] = get_coor(row[2])
-            print("Update", row[2], address_dic[row[2]])
-
-        if address_dic[row[2]][0] != 0 and address_dic[row[2]][1]!=0:
-            print("Good", row[2], address_dic[row[2]])
+                address_dic[row[2]] = get_coor(row[2])
+                print("Update", row[2], address_dic[row[2]])
+                if cnt % 5 == 0:
+                    print("[cnt]"+str(cnt))
+                    save_data()
+        if address_dic[row[2]][0] != 0 and address_dic[row[2]][1] != 0:
+            # print("Good", row[2], address_dic[row[2]])
             demo_address_dic[row[2]] = address_dic[row[2]]
             dic.append({
                 "id":row[0],
@@ -85,16 +99,16 @@ def update_mask():
                 "adult_count":row[4],
                 "child_count": row[5],
                 "updatetime": row[6]
-    })
+            })
     # print(dic)
 
 def load_data():
-    # with open('data.json', 'r') as json_file:
-    #     global dic
-    #     dic = json.load(json_file)
-    # with open('demo_address.json', 'r') as json_file:
-    #     global demo_address_dic
-    #     demo_address_dic = json.load(json_file)
+    with open('data.json', 'r') as json_file:
+        global tmp_dic
+        tmp_dic = json.load(json_file)
+    with open('demo_address.json', 'r') as json_file:
+        global demo_address_dic
+        demo_address_dic = json.load(json_file)
     with open('address.json', 'r') as json_file:
         global address_dic
         address_dic = json.load(json_file)
@@ -132,10 +146,21 @@ def get_coor_google(addr):
     else:
         error_cnt += 1
         return (0,0)
-    
+
+
 load_data()
+ori_address_dic = len(address_dic)
+ori_data_dic = len(tmp_dic)
+print("address_dic:", ori_address_dic)
+print("data_dic:", ori_data_dic)
 update_mask()
-save_data()
+
+print("ori_address_dic:", ori_address_dic)
+print("ori_data_dic:", ori_data_dic)
+print("address_dic:", len(address_dic))
+print("data_dic:", len(dic))
 # get_coor_google("新北市平溪區公園街17號1樓")
 
-
+for i in address_dic.item():
+    if 
+save_data()
